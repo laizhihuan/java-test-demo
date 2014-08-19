2343bc026e7b7d0b3c7b42fb67151306e71a14a1
laizhihuan <kojavaee@qq.com>
2014-08-19 21:20:38 +0800

    添加下载计数器

---
+++ com.kojavaee/src/main/java/com/kojavaee/downloads/Downloads.java
@@ -0,0 +1,15 @@
+package com.kojavaee.downloads;
+
+public class Downloads {
+	
+	public static void main(String[] args) {
+		int num = 10;
+		int total = 10000;
+		
+		int count = 2;
+		for(int i=0; i<10; i++) {
+			
+		}
+		
+	}
+}
