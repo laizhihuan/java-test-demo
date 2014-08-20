fe5a3c4074fc9bfd681ec3425ce23c2020684e5c
laizhihuan <kojavaee@qq.com>
2014-08-20 14:53:26 +0800

    等差数列

--- com.kojavaee/src/main/java/com/kojavaee/downloads/Downloads.java
+++ com.kojavaee/src/main/java/com/kojavaee/downloads/Downloads.java
@@ -3,13 +3,19 @@ package com.kojavaee.downloads;
 public class Downloads {
 	
 	public static void main(String[] args) {
-		int num = 10;
-		int total = 10000;
+		int sn = 10000;
+		int a1 = 100;
+		int n = 30;
+		int an = 2*sn/n -a1;
+		double d = (an - a1)/(n-1);
 		
-		int count = 2;
-		for(int i=0; i<10; i++) {
-			
+		double total = 0;
+		for(int i=1; i<=n; i++) {
+			double ai = a1 + (i-1)*d;
+			total += ai;
+			System.out.println("n: " + i + "  " + ai);
 		}
-		
+		System.out.println(total);
+		System.out.println("d " + d + " an " + an);
 	}
 }
