5f2f571c1bfc221362cd753f46095f9f39e900f7
laizhihuan <kojavaee@qq.com>
2014-08-23 09:03:40 +0800

    一道面试题

---
+++ com.kojavaee/src/main/java/com/kojavaee/TestOutput.java
@@ -0,0 +1,22 @@
+package com.kojavaee;
+
+public class TestOutput {
+	static int[] a = new int[3];
+
+	public static void main(String[] args) throws Exception
+	{
+		for (int i = 0; i < 3; i++) 
+		{
+			Chek(i*2);
+		}
+	}
+
+	static void Chek(int b)
+	{
+		for (int i = a.length; i > 0; i--) 
+		{
+			a[i-1]+=b+i;
+			System.out.println("aa :"+a[i-1]);
+		}
+	} 
+}
