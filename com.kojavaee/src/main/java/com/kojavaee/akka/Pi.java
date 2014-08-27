b702dfcbd0577e8b83a1b9e7ea8971513be97ccc
laizhihuan <kojavaee@qq.com>
2014-08-27 11:18:07 +0800

    完善akka示例

--- com.kojavaee/src/main/java/com/kojavaee/akka/Pi.java
+++ com.kojavaee/src/main/java/com/kojavaee/akka/Pi.java
@@ -4,17 +4,24 @@
 package com.kojavaee.akka;
 
 
+import java.util.concurrent.TimeUnit;
+
 import scala.concurrent.duration.Duration;
+import akka.actor.Actor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import akka.actor.UntypedActorFactory;
+import akka.routing.RoundRobinRouter;
 
 public class Pi {
 	
 	public static void main(String[] args) {
 		Pi pi = new Pi();
 		pi.calculate(4,10000,10000);
+//		Pi approximation: 		3.1415926435897883
+//		Calculation time: 	515 milliseconds
 	}
 	
 	static class Calculate {
@@ -71,16 +78,77 @@ public class Pi {
 		
 		@Override
 		public void onReceive(Object message) throws Exception {
-			
+			if(message instanceof Work) {
+				Work work = (Work)message;
+				double result = calculatePiFor(work.getStart(),work.getNrOfElements());
+				getSender().tell(new Result(result), getSelf());
+			} else {
+				unhandled(message);
+			}
 		}
+
+		private double calculatePiFor(int start, int nrOfElements) {
+			double acc = 0.0;
+			for(int i=start*nrOfElements; i<=((start+1) * nrOfElements - 1); i++) {
+				acc += 4.0 * ( 1 - (i % 2) * 2) / (2 * i + 1);
+			}
+			return acc;
+		}
+	}
+	
+	public static class Master extends UntypedActor {
+		private final int nrOfMessage;
+		private final int nrOfElements;
 		
+		private double pi;
+		private int nrOfResults;
+		private final long start = System.currentTimeMillis();
+		
+		private final ActorRef listener;
+		private final ActorRef workerRouter;
+		
+		public Master(int nrOfWorkers,int nrOfMessages,int nrOfElements,ActorRef listener) {
+			this.nrOfElements = nrOfElements;
+			this.nrOfMessage = nrOfMessages;
+			this.listener = listener;
+			
+			 workerRouter = this.getContext().actorOf(new Props(Worker.class).withRouter(new RoundRobinRouter(nrOfWorkers)),
+			          "workerRouter");
+		}
+
+		@Override
+		public void onReceive(Object message) throws Exception {
+			if(message instanceof Calculate) {
+				for(int start = 0; start<nrOfMessage; start++) {
+					workerRouter.tell(new Work(start,nrOfElements), getSelf());
+				}
+			} else if (message instanceof Result) {
+				Result result = (Result) message;
+				pi += result.getValue();
+				nrOfResults += 1;
+				if(nrOfResults == nrOfMessage) {
+					Duration duration = Duration.create(System.currentTimeMillis() - start,TimeUnit.MILLISECONDS);
+					listener.tell(new PiApproximation(pi, duration), getSelf());
+					
+					getContext().stop(getSelf());
+				}
+			} else {
+				unhandled(message);
+			}
+		}
 	}
 	
 	public static class Listener extends UntypedActor {
 
 		@Override
 		public void onReceive(Object message) throws Exception {
-			
+			if(message instanceof PiApproximation) {
+				PiApproximation approximation = (PiApproximation)message;
+				System.out.println(String.format("\n\tPi approximation: \t\t%s\n\tCalculation time: \t%s",  approximation.getPi(), approximation.getDuration()));
+				getContext().system().shutdown();
+			} else {
+				unhandled(message);
+			}
 		}
 		
 	}
@@ -88,6 +156,16 @@ public class Pi {
 	private void calculate(final int nrOfWorkers, final int nrOfElements, final int nrOfMessage) {
 		ActorSystem system = ActorSystem.create("PiSystem");
 		
-		final ActorRef listener = system.actorOf(new Props(null, Listener.class, null),"listener");
+		final ActorRef listener = system.actorOf(new Props(Listener.class),"listener");
+		
+		ActorRef master = system.actorOf(new Props(new UntypedActorFactory() {
+			
+			@Override
+			 public UntypedActor create() {
+		        return new Master(nrOfWorkers, nrOfMessage, nrOfElements, listener);
+		      }
+		}));
+		
+		master.tell(new Calculate());
 	}
 }
