f5e5530e1931a9ba290320c9d3538292b5152d57
laizhihuan <kojavaee@qq.com>
2014-09-17 18:04:59 +0800

    主事件循环和发送聊天消息

--- com.kojavaee/src/main/java/com/kojavaee/jgroup/SimpleChat.java
+++ com.kojavaee/src/main/java/com/kojavaee/jgroup/SimpleChat.java
@@ -1,6 +1,10 @@
 package com.kojavaee.jgroup;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.jgroups.JChannel;
+import org.jgroups.Message;
 
 /**
  * http://www.oschina.net/translate/jgroups-writing-a-simple-application
@@ -15,6 +19,30 @@ public class SimpleChat {
 	private void start() throws Exception {
 		channel = new JChannel(); //use the default config, udp.xml
 		channel.connect("ChatCluster");
+		eventLoop();
+		channel.close();
+	}
+	
+	private void eventLoop() {
+		 
+	    BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+	 
+	    while(true) {
+	        try {
+	            System.out.print("> "); System.out.flush();
+	            String line=in.readLine().toLowerCase();
+	            if(line.startsWith("quit") || line.startsWith("exit"))
+	                break;
+	 
+	            line="[" + user_name + "] " + line;
+	            Message msg=new Message(null, null, line);
+	            channel.send(msg);
+	 
+	        }
+	 
+	        catch(Exception e) {
+	        }
+	    }
 	}
 	
 	public static void main(String[] args) throws Exception {
