9aa52a8f993e5c298a72cfe3fd436555e1e14290
laizhihuan <kojavaee@qq.com>
2014-09-17 18:04:10 +0800

    创建并加入组通道

--- com.kojavaee/src/main/java/com/kojavaee/jgroup/SimpleChat.java
+++ com.kojavaee/src/main/java/com/kojavaee/jgroup/SimpleChat.java
@@ -1,5 +1,23 @@
 package com.kojavaee.jgroup;
 
-public class SimpleChat {
+import org.jgroups.JChannel;
 
+/**
+ * http://www.oschina.net/translate/jgroups-writing-a-simple-application
+ * @author zhihuanglai
+ *
+ */
+public class SimpleChat {
+	
+	JChannel channel;
+	String user_name = System.getProperty("user.name","n/a");
+	
+	private void start() throws Exception {
+		channel = new JChannel(); //use the default config, udp.xml
+		channel.connect("ChatCluster");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new SimpleChat().start();
+	}
 }
