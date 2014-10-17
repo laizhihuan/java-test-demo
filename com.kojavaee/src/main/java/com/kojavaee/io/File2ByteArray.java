7ead182e64d06080f9de988323a8b45abe5d522b
laizhihuan <kojavaee@qq.com>
2014-10-17 10:14:38 +0800

    Mapped File way MappedByteBuffer 可以在处理大文件时，提升性能

--- com.kojavaee/src/main/java/com/kojavaee/io/File2ByteArray.java
+++ com.kojavaee/src/main/java/com/kojavaee/io/File2ByteArray.java
@@ -93,4 +93,35 @@ public class File2ByteArray {
             }
         }
     }
+    /**
+     * Mapped File way MappedByteBuffer 可以在处理大文件时，提升性能
+     * 
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    public static byte[] toByteArray3(String filename) throws IOException {
+
+        FileChannel fc = null;
+        try {
+            fc = new RandomAccessFile(filename, "r").getChannel();
+            MappedByteBuffer byteBuffer = fc.map(MapMode.READ_ONLY, 0, fc.size()).load();
+            System.out.println(byteBuffer.isLoaded());
+            byte[] result = new byte[(int) fc.size()];
+            if (byteBuffer.remaining() > 0) {
+                // System.out.println("remain");
+                byteBuffer.get(result, 0, byteBuffer.remaining());
+            }
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                fc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
