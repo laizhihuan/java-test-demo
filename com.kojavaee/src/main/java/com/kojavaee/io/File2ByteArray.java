2164cd46c498318cb50360427cead5e36270fc0c
laizhihuan <kojavaee@qq.com>
2014-10-17 09:28:27 +0800

    Nio way

--- com.kojavaee/src/main/java/com/kojavaee/io/File2ByteArray.java
+++ com.kojavaee/src/main/java/com/kojavaee/io/File2ByteArray.java
@@ -7,6 +7,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 
 public class File2ByteArray {
     /**
@@ -44,6 +49,48 @@ public class File2ByteArray {
                 e.printStackTrace();
             }
             bos.close();
+        }
+    }
+
+    /**
+     * NIO way
+     * 
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    public static byte[] toByteArray2(String filename) throws IOException {
+
+        File f = new File(filename);
+        if (!f.exists()) {
+            throw new FileNotFoundException(filename);
+        }
+
+        FileChannel channel = null;
+        FileInputStream fs = null;
+        try {
+            fs = new FileInputStream(f);
+            channel = fs.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
+            while ((channel.read(byteBuffer)) > 0) {
+                // do nothing
+                // System.out.println("reading");
+            }
+            return byteBuffer.array();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fs.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
