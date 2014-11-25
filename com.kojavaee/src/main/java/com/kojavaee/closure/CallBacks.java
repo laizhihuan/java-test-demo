8f3a8c54557821bb872012a46e30e5a74a212cfa
laizhihuan <kojavaee@qq.com>
2014-11-25 09:09:24 +0800

    添加闭包回调的comment

--- com.kojavaee/src/main/java/com/kojavaee/closure/CallBacks.java
+++ com.kojavaee/src/main/java/com/kojavaee/closure/CallBacks.java
@@ -14,7 +14,7 @@ class Callee1 implements Incrementable {
     }
 }
 
-class MyIncrement implements Incrementable {
+class MyIncrement {
     public void increment() {
         System.out.println("Other operation");
     }
@@ -32,7 +32,8 @@ class Callee2 extends MyIncrement {
         System.out.println(i);
     }
     
-    //
+    //内部类closure实现了 incrementable,提供一个返回Callee2的“钩子” (hook)，而且是安全的钩子。
+    //无论谁获得此Incrementable的引用，都只能调用increment(),除此之武器没有其他功能。
     private class Closure implements Incrementable {
         public void increment() {
             Callee2.this.increment();
@@ -44,6 +45,8 @@ class Callee2 extends MyIncrement {
     }
 }
 
+//Caller的构造器需要一个Incrementable的引用作为参数(可以在任意时刻普获回调的引用)，
+//然后在以后的某个时刻，Caller对象可以使用此引用回调Callee类
 class Caller {
     private Incrementable callbackReference;
     Caller(Incrementable cbh) {
@@ -59,7 +62,7 @@ public class CallBacks {
     public static void main(String[] args) {
         Callee1 c1 = new Callee1();
         Callee2 c2 = new Callee2();
-        
+        //传参数时传入引用,这个引用就叫闭包
         MyIncrement.f(c2);
         
         Caller caller1 = new Caller(c1);
