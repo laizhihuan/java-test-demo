3d47655e53bf9f1eb8d5b7a7f7c8b0b3f3271068
laizhihuan <kojavaee@qq.com>
2015-02-27 15:25:51 +0800

    guava牛逼的方法

--- com.kojavaee/src/main/java/com/kojavaee/guava/GuavaAPI.java
+++ com.kojavaee/src/main/java/com/kojavaee/guava/GuavaAPI.java
@@ -17,4 +17,21 @@ public class GuavaAPI {
         System.out.println(outputStr2);
         System.out.println(outputStr3);
     }
+    
+    /*
+     * 第一个技巧是使用 if 和 while 来实现了比较优雅的分隔符拼接，避免了在末尾插入分隔符的尴尬；
+     * 第二个技巧是使用了自定义的 toString 方法而不是 Object#toString 来将对象序列化成字符串，为后续的各种空指针保护开了方便之门。
+     * 
+    public <A extends Appendable> A appendTo(A appendable, Iterator<?> parts) throws IOException {
+        checkNotNull(appendable);
+        if (parts.hasNext()) {
+          appendable.append(toString(parts.next()));
+          while (parts.hasNext()) {
+            appendable.append(separator);
+            appendable.append(toString(parts.next()));
+          }
+        }
+        return appendable;
+      }
+     */              
 }
