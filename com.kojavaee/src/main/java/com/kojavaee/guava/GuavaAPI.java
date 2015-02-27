c67ecfc50d0bb67bc3b54e6db83bf7f27f43044f
laizhihuan <kojavaee@qq.com>
2015-02-27 15:00:11 +0800

    add joiner

--- com.kojavaee/src/main/java/com/kojavaee/guava/GuavaAPI.java
+++ com.kojavaee/src/main/java/com/kojavaee/guava/GuavaAPI.java
@@ -6,7 +6,13 @@ public class GuavaAPI {
     public static void main(String[] args) {
         //[1,2,3]拼接成1 2 3
         String outputStr = Joiner.on(' ').join(1,2,3);
-        System.out.println(outputStr);
         
+        String outputStr1 = Joiner.on(' ').skipNulls().join(1,null,3);
+        String outputStr2 = Joiner.on(' ').useForNull("none").join(1,null,3);
+        
+        
+        System.out.println(outputStr);
+        System.out.println(outputStr1);
+        System.out.println(outputStr2);
     }
 }
